7336b354f0c33a213472cef62178dd87e0a6e6fc
fkorf <sourceforge@emjoy.net>
2017-01-27 22:32:44 +0100

    livestats.php mit Filterung auf Zeitraum

--- Beispiele/livestats.php
+++ Beispiele/livestats.php
@@ -4,6 +4,31 @@
 include("config.php");
 include("lautfmadmin.php");
 
+$defaultTimespan = 1; // Standard: 0) Alle, 1) Aktuelle Woche, 2) Letzte Wcohe
+$displayTimespanSelector = 1; // Anzeige der Zeitraumauswahl - 0) nein, 1) ja
+
+// Bestimmung des Zeitpunkts des letzten Montags, 0:00 Uhr
+function getStartOfWeek() {
+  $daysAfterMonday = date('w') - 1;
+  if($daysAfterMnoday == -1) $daysAfterMonday = 6;
+  $mon =  time() - ($daysAfterMonday * 60 * 60 * 24) - date('G') * 60 * 60 - date('i') * 60;
+  return $mon;
+}
+
+// Festlegen des Start-/Endzeitpunkts fuer die Filterung
+$minTime = 0;
+$maxTime = time();
+
+$timespan = isset($_GET['zeitraum']) ? $_GET['zeitraum'] : $defaultTimespan;
+if($timespan > 0) {
+	$weeks = ($timespan - 1) * 60 * 60 * 24 * 7; 
+	$minTime = getStartOfWeek() - $weeks;
+	$maxTime = $minTime + 60 * 60 * 24 * 7;
+}
+$selected = array();
+$selected[$timespan]="selected";
+
+// API-Call
 $lfm = new LautfmAdmin();
 $lfm->token = $token;
 $logs = $lfm->getLiveLog($station);
@@ -16,10 +41,24 @@ $logs = $lfm->getLiveLog($station);
 		<link rel="stylesheet" href="lautfmadmin.css">
 	</head>
 	<body>
-	
+		
+	<form>
 	<table class='lfmadmin'>
 	  <tr class='lfmtitle'>
-			<td colspan=4 style="padding:5pt">Live-Sessions</td>
+			<td colspan=4 style="padding:5pt" style='vertical-align:middle'>
+				Live-Sessions
+				<?
+				if($displayTimespanSelector) {
+					?>
+					<select name='zeitraum' size=1 onchange="this.form.submit();" style='float:right'>
+						<option value='0' <? echo $selected[0] ?>>&nbsp;Alle&nbsp;</option>
+						<option value='1' <? echo $selected[1] ?>>&nbsp;Aktuelle Woche&nbsp;</option>
+						<option value='2' <? echo $selected[2] ?>>&nbsp;Letzte Woche&nbsp;</option>
+					</select>
+				  <?
+ 			  }
+				?>
+			</td>
 	  </tr>
 		<tr class="lfmhead">
 			<td class='lfmhead'>Datum</td>
@@ -30,14 +69,16 @@ $logs = $lfm->getLiveLog($station);
 		<?
 		$total = 0;
 		for($i = 0; $i < count($logs); $i++) {
-			echo "<tr class='lfmcontent'>";
-			echo "<td class='lfmcontent'>".$logs[$i]->getDayAsString()."</td>";
-			echo "<td class='lfmcontent'>".$logs[$i]->getStartTimeAsString()." Uhr</td>";
-			echo "<td class='lfmcontent'>".$logs[$i]->getEndTimeAsString()." Uhr</td>";
-			echo "<td class='lfmcontent'>".$logs[$i]->getDurationAsString()."</td>";
-			echo "</tr>";
+			if($logs[$i]->end >= $minTime && $logs[$i]->end <= $maxTime) {
+				echo "<tr class='lfmcontent'>";
+				echo "<td class='lfmcontent'>".$logs[$i]->getDayAsString()."</td>";
+				echo "<td class='lfmcontent'>".$logs[$i]->getStartTimeAsString()." Uhr</td>";
+				echo "<td class='lfmcontent'>".$logs[$i]->getEndTimeAsString()." Uhr</td>";
+				echo "<td class='lfmcontent'>".$logs[$i]->getDurationAsString()."</td>";
+				echo "</tr>";
+				$total += $logs[$i]->getDuration();
+  		}
 			
-			$total += $logs[$i]->getDuration();
 		}
 	  echo "<tr class='lfmcontent'>";
 	  echo "<td class='lfmcontent' colspan=3>Gesamt</td>";
@@ -45,7 +86,7 @@ $logs = $lfm->getLiveLog($station);
 	  echo "</tr>";
 		?>
 	</table>	
-		
+  </form>		
 		
 	</body>
 </html>
